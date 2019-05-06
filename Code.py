1f18e06e4ee9ff6e2a3c0010f665aa54a4007752
Sarshar Imtiaz <50096024+SarsharImtiaz@users.noreply.github.com>
2019-05-06 17:01:01 +0500

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,20 @@
+print('CALCULATOR')
+function = input('Select the Function which you wanna perform + , - , * , / ')
+No1=int(input('Enter 1st Number: '))
+No2=int(input('Enter 2nd Number: '))
+if function == '+':
+    {
+        print (No1+No2)
+    }
+elif function == '-':
+    {
+        print (No1-No2)
+    }
+elif function == '*':
+    {
+        print (No1*No2)
+    }
+elif function == '/':
+    {
+        print (No1/No2)
+    } 
